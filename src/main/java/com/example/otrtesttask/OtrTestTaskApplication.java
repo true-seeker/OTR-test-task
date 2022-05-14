06bfbe7e4849a4480b8bbb79b3f75ad649136782
TrueSeeker322 <murz.kirill@yandex.ru>
2022-05-14 13:08:20 +0500

    initial commit

---
+++ src/main/java/com/example/otrtesttask/OtrTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.otrtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtrTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtrTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/otrtesttask/OtrTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otrtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtrTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
